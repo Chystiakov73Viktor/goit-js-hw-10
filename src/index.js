1303bdb3c32332541e3bdaf0834ceef35c0d23f7
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-09-05 17:02:32 +0300

    додано дз

--- src/index.js
+++ src/index.js
@@ -1,7 +1,80 @@
-import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
-axios.defaults.headers.common["x-api-key"] = "live_RUkNN0NvPlCPIiuiy2IQkYIXpUW9wlCvi9qmbogKRYu2UpebzmJs94dCYv2mjLZ5";
+const selectEl = document.querySelector('.breed-select');
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
+const catInfoEl = document.querySelector('.cat-info');
 
+selectEl.style.width = '180px';
+catInfoEl.style.width = '400px';
 
+errorEl.hidden = true;
 
+fetchBreeds()
+  .then(response => {
+    console.log(response);
+    const { data } = response;
+    return data;
+  })
+  .then(data => {
+    catIdMarkup(data);
+    new SlimSelect({
+      select: '.breed-select',
+    });
+  })
+  .catch(monitorCondition);
 
+selectEl.addEventListener('change', onSelect);
+
+function onSelect(evt) {
+  catInfoEl.hidden = true;
+  loaderEl.hidden = false;
+  const catId = evt.target.value;
+  if (catId) {
+    return fetchCatByBreed(catId)
+      .then(response => {
+        const { data } = response;
+        return data;
+      })
+      .then(data => {
+        catMarkupImg(data);
+        loaderEl.hidden = true;
+        catInfoEl.hidden = false;
+      })
+      .catch(monitorCondition);
+  }
+}
+
+function monitorCondition() {
+  selectEl.hidden = true;
+  loaderEl.hidden = true;
+  errorEl.hidden = false;
+}
+
+function catMarkupImg(data) {
+  const markup = data
+    .map(el => {
+      return `
+            <div class="box-img">
+            <img src="${el.url}" alt="${el.breeds[0].name}" width="400" height="200" /> 
+            <h2>${el.breeds[0].name}</h2>
+            <p>${el.breeds[0].description}</p>
+            <p><b>Temperament:</b> ${el.breeds[0].temperament}</p>
+            </div>
+            `;
+    })
+    .join('');
+
+  catInfoEl.innerHTML = markup;
+}
+
+function catIdMarkup(data) {
+  data.map(cat => {
+    selectEl.insertAdjacentHTML(
+      'beforeend',
+      `<option value="${cat.id}">${cat.name}</option>`
+    );
+  });
+}
